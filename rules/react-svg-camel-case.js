9d4452e7ef6b35f180a55e61fc9a4174d88b11de
Derek Foster <derekfoster94@gmail.com>
2022-12-08 13:08:14 -0800

    able to detect when dashes exist on prop in JSX tags

--- rules/react-svg-camel-case.js
+++ rules/react-svg-camel-case.js
@@ -3,25 +3,77 @@
  * @author Derek Foster
  */
 
-'use strict'
+"use strict";
 
 //------------------------------------------------------------------------------
 // Rule Definition
 //------------------------------------------------------------------------------
 
 const BLOCK_DEFAULTS = [
-  'describe',
-  'it',
-  'context',
-  'test',
-  'tape',
-  'fixture',
-  'serial',
-]
-const FOCUS_DEFAULTS = ['only']
+  "describe",
+  "it",
+  "context",
+  "test",
+  "tape",
+  "fixture",
+  "serial",
+];
+const FOCUS_DEFAULTS = ["only"];
 
 module.exports = {
-  create(context) {
-    console.log(context)
+  meta: {
+    type: "problem",
+    messages: {
+      dashedProps: "JSX: found dashes on prop {{ propName }} on {{ tagName }}.",
+    },
   },
-}
+  create(context) {
+    function getPropName(propNode) {
+      switch (propNode.type) {
+        case "JSXSpreadAttribute":
+          return context.getSourceCode().getText(propNode.argument);
+        case "JSXIdentifier":
+          return propNode.name;
+        case "JSXMemberExpression":
+          return `${getPropName(propNode.object)}.${propNode.property.name}`;
+        default:
+          return propNode.name
+            ? propNode.name.name
+            : `${context.getSourceCode().getText(propNode.object)}.${
+                propNode.property.name
+              }`; // needed for typescript-eslint parser
+      }
+    }
+
+    function getJSXTagName(jsxNode) {
+      switch (jsxNode.type) {
+        case "JSXOpeningElement":
+          return jsxNode.name.name;
+        default:
+          return "";
+      }
+    }
+
+    return {
+      JSXOpeningElement: (node) => {
+        node.attributes.forEach((attr) => {
+          const propName = getPropName(attr);
+
+          if (propName.includes("-")) {
+            context.report({
+              node,
+              messageId: "dashedProps",
+              data: {
+                propName,
+                tagName: getJSXTagName(node),
+              },
+              fix(fixer) {
+                return null;
+              },
+            });
+          }
+        });
+      },
+    };
+  },
+};
